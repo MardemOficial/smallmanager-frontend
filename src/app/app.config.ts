a255c9d0acb466f77b8c47ce74fee0c2b6bb537a
Mardem Paz <mardenexecutive@gmail.com>
2025-11-11 21:57:50 -0300

    feat: Configure internationalization (i18n)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { PaginationClass } from './classes/pagination.class';
 
 import { routes } from './app.routes';
 
@@ -9,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: MatPaginatorIntl, useClass: PaginationClass }
   ]
 };
