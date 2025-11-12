9a156ec638b2d55cf870bd218d065c7de7164503
Mardem Paz <mardenexecutive@gmail.com>
2025-11-12 11:19:44 -0300

    feat: Add benefits management pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,24 +4,42 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProdutoComponent } from './pages/produto/produto.component';
 import { UserListComponent } from './pages/user/user-list/user-list.component';
 import { UserFormComponent } from './pages/user/user-form/user-form.component';
-import { HomeComponent } from './shared/home/home.component'
+import { HomeComponent } from './shared/home/home.component';
 import { PositionListComponent } from './pages/position/position-list.component/position-list.component';
 import { PositionFormComponent } from './pages/position/position-form.component/position-form.component';
+import { BenefitListComponent } from './pages/benefit/benefit-list.component/benefit-list.component';
+import { BenefitFormComponent } from './pages/benefit/benefit-form.component/benefit-form.component';
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-    
-    {path: '', component: LoginComponent},
-    {path: 'home' ,component: HomeComponent, children: [
-           {path: 'dashboard', component: DashboardComponent},
-           {path: 'produto', component: ProdutoComponent},
-           {path: 'user', children: [
-                { path: '', component: UserListComponent },
-                { path: 'form', component: UserFormComponent },
-           ]},
-            {path: 'position', children: [
-                { path: '', component: PositionListComponent },
-                { path: 'form', component: PositionFormComponent },
-           ]},
-    ]},
+  { path: '', component: LoginComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'produto', component: ProdutoComponent },
+      {
+        path: 'user',
+        children: [
+          { path: '', component: UserListComponent },
+          { path: 'form', component: UserFormComponent },
+        ],
+      },
+      {
+        path: 'position',
+        children: [
+          { path: '', component: PositionListComponent },
+          { path: 'form', component: PositionFormComponent },
+        ],
+      },
+      {
+        path: 'benefit',
+        children: [
+          { path: '', component: BenefitListComponent },
+          { path: 'form', component: BenefitFormComponent },
+        ],
+      },
+    ],
+  },
 ];
